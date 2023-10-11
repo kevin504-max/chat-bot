576f1e715841416dd4b489341282c04d89c7aa0f
Kevin Lucas <kevinbrito2012@gmail.com>
2023-10-11 19:26:32 -0300

    feat: configure initial main js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import { createRouter, createWebHistory } from 'vue-router'
+import VueSweetalert2 from 'vue-sweetalert2'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'sweetalert2/dist/sweetalert2.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+const app = createApp(App)
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        // define all routes here....
+    ],
+})
+
+app.config.productionTip = false
+
+app.use(router)
+app.use(VueSweetalert2)
+
+axios.defaults.baseURL = 'http://localhost:3300/api'
+
+app.mount('#app')
