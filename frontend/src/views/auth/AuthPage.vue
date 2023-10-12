865913d5e4863b6f8632ef0a4ac0018224ec1606
Kevin Lucas <kevinbrito2012@gmail.com>
2023-10-12 11:49:32 -0300

    reafact: make auth pages with composition api

--- frontend/src/views/auth/AuthPage.vue
+++ frontend/src/views/auth/AuthPage.vue
@@ -6,104 +6,119 @@
           <div class="card-body p-md-5">
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                
                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login</p>
-                
-                <form @submit.stop.prevent="login" class="mx-1 mx-md-4">
-                  
+                <form @submit.prevent="login" class="mx-1 mx-md-4">
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input 
+                      <input
                       v-model="email"
-                      type="email" id="form3Example3c" class="form-control"
-                      placeholder="Your e-mail here..."    
+                      type="email"
+                      id="form3Example3c"
+                      class="form-control"
+                      placeholder="Your e-mail here..."
                       />
                       <label class="form-label mt-3" for="form3Example3c">E-mail</label>
                     </div>
                   </div>
-                  
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input 
+                      <input
                       v-model="password"
-                      type="password" id="form3Example4c" class="form-control"
-                      placeholder="*********"    
+                      type="password"
+                      id="form3Example4c"
+                      class="form-control"
+                      placeholder="*********"
                       />
                       <label class="form-label mt-3" for="form3Example4c">Password</label>
                     </div>
                   </div>
-                  
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4 gap-3">
-
                     <button type="submit" class="btn btn-primary btn-lg">Login</button>
-                    <button type="button" class="btn btn-outline-primary btn-lg" @click="$router.push({ name: 'register' })">Register</button>
-
-                  </div>
-                  
-                </form>
-                
-              </div>
-              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                class="img-fluid" alt="Sample image">
-                
-              </div>
-              
+                    <button
+                    type="button"
+                    class="btn btn-outline-primary btn-lg"
+                    @click="goToRegister"
+                    >
+                    Register
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+              <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+              class="img-fluid"
+              alt="Sample image"
+              />
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
-import axios from 'axios'
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter} from "vue-router";
+import Swal from "sweetalert2";
 
 export default {
-  name: 'AuthPage',
-  data () {
-    return {
-      email: '',
-      password: ''
-    }
-  },
-  methods: {
-    login () {
-      const payload = {
-        email: this.email,
-        password: this.password
-      }
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const router = useRouter();
 
-      axios.post('login', payload, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-      }).then((response) => {
-          this.$swal({
-              icon: 'success',
-              title: 'Success!',
-              text: response.data.message,
-              showConfirmButton: false,
-              timer: 2000
+    const login = () => {
+      const payload = {
+        email: email.value,
+        password: password.value,
+      };
+
+      axios
+        .post("login", payload, {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        })
+        .then((response) => {
+          Swal.fire({
+            icon: "success",
+            title: "Success!",
+            text: response.data.message,
+            showConfirmButton: false,
+            timer: 2000,
           });
-          this.$router.push({ name: 'chat' });
-      }).catch((error) => {
-          this.$swal({
-              icon: 'error',
-              title: 'Oops...',
-              text: error.response.data.message,
-              showConfirmButton: false,
-              timer: 2000
+          router.push({ name: "chat" });
+        })
+        .catch((error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: error.response.data.message,
+            showConfirmButton: false,
+            timer: 2000,
           });
-      });
-    }
-  }
-}
+        });
+    };
+
+      const goToRegister = () => {
+        router.push({ name: "register" });
+      };
+
+      return {
+        email,
+        password,
+        login,
+        goToRegister,
+      };
+  },
+};
 </script>
 
 <style scoped></style>
--- frontend/src/views/auth/RegisterPage.vue
+++ frontend/src/views/auth/RegisterPage.vue
@@ -6,127 +6,152 @@
                     <div class="card-body p-md-5">
                         <div class="row justify-content-center">
                             <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                                
                                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Register</p>
-                                
-                                <form class="mx-1 mx-md-4" @submit.stop.prevent="register">
-                                    
+                                <form @submit.prevent="register" class="mx-1 mx-md-4">
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline flex-fill mb-0">
-                                            <input 
+                                            <input
                                             v-model="username"
-                                            type="text" id="form3Example1c" class="form-control" 
+                                            type="text"
+                                            id="form3Example1c"
+                                            class="form-control"
                                             placeholder="Type your username..."
                                             />
                                             <label class="form-label mt-3" for="form3Example1c">Username</label>
                                         </div>
                                     </div>
-                                    
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline flex-fill mb-0">
-                                            <input 
+                                            <input
                                             v-model="email"
-                                            type="email" id="form3Example3c" class="form-control"
+                                            type="email"
+                                            id="form3Example3c"
+                                            class="form-control"
                                             placeholder="Your e-mail here..."
                                             />
                                             <label class="form-label mt-3" for="form3Example3c">E-mail</label>
                                         </div>
                                     </div>
-                                    
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline flex-fill mb-0">
-                                            <input 
+                                            <input
                                             v-model="password"
-                                            type="password" id="form3Example4c" class="form-control" 
+                                            type="password"
+                                            id="form3Example4c"
+                                            class="form-control"
                                             placeholder="Your password here..."
                                             />
                                             <label class="form-label mt-3" for="form3Example4c">Password</label>
                                         </div>
                                     </div>
-
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline flex-fill mb-0">
-                                            <input 
+                                            <input
                                             v-model="confirmPassword"
-                                            type="password" id="form3Example4c" class="form-control" 
+                                            type="password"
+                                            id="form3Example5c"
+                                            class="form-control"
                                             placeholder="Confirm your password..."
                                             />
-                                            <label class="form-label mt-3" for="form3Example4c">Confim Password</label>
+                                            <label class="form-label mt-3" for="form3Example5c">Confirm Password</label>
                                         </div>
                                     </div>
-                                    
                                     <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4 gap-3">
-
                                         <button type="submit" class="btn btn-primary btn-lg">Register</button>
-                                        <button type="button" class="btn btn-outline-primary btn-lg" @click="$router.push({ name: 'login' })">Login</button>
-                                    </div>
-                                    
-                                </form>
-                                
-                            </div>
-                            <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                                
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                                class="img-fluid" alt="Sample image">
-                                
-                            </div>
+                                        <button
+                                        type="button"
+                                        class="btn btn-outline-primary btn-lg"
+                                        @click="goToLogin"
+                                        >
+                                        Login
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                            <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                            class="img-fluid"
+                            alt="Sample image"
+                            />
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </template>
 
 <script>
-import axios from 'axios'
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import Swal from "sweetalert2";
 
 export default {
-    name: 'RegisterPage',
-    data () {
-        return {
-            'name': '',
-            'email': '',
-            'password': '',
-            'confirmPassword': ''
-        };
-    },
-    methods: {
-        register () {
-           const payload = {
-                username: this.username,
-                email: this.email,
-                password: this.password,
-                confirmPassword: this.confirmPassword,
+    setup() {
+        const username = ref("");
+        const email = ref("");
+        const password = ref("");
+        const confirmPassword = ref("");
+        const router = useRouter();
+
+        const register = () => {
+            const payload = {
+                username: username.value,
+                email: email.value,
+                password: password.value,
+                confirmPassword: confirmPassword.value,
             };
             
-            axios.post('register', payload).then((response) => {
-                this.$swal({
-                    icon: 'success',
-                    title: 'Sucess!',
-                    text: response.data.message,
-                    showConfirmButton: false,
-                    timer: 2500
+            axios
+                .post("register", payload, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
                 })
-                setTimeout(() => {
-                    this.$router.push({ name: 'chat' })
-                }, 2000);
-            }).catch((error) => {
-                this.$swal({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: error.response.data.message,
-                    showConfirmButton: false,
-                    timer: 2500
+                .then((response) => {
+                    Swal.fire({
+                        icon: "success",
+                        title: "Success!",
+                        text: response.data.message,
+                        showConfirmButton: false,
+                        timer: 2000,
+                    }).then(() => {
+                        router.push({ name: "chat" });
+                    });
                 })
-            });
-        },
-    }
-}
+                .catch((error) => {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: error.response.data.message,
+                        showConfirmButton: false,
+                        timer: 2500,
+                    });
+                });
+        }
+
+        const goToLogin = () => {
+            router.push({ name: "login" });
+        };
+
+        return {
+            username,
+            email,
+            password,
+            confirmPassword,
+            register,
+            goToLogin,
+        };
+    },  
+};
 </script>
 
 <style scoped></style>
