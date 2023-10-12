5ea8160b1fc02a5c9c8200400cbb1e7f5972cf3a
Kevin Lucas <kevinbrito2012@gmail.com>
2023-10-12 11:07:10 -0300

    feat: make user controller

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,63 @@
+const { UserService } = require('../services');
+const userService = new UserService();
+
+module.exports = class UserController {
+    registerUser = async (request, response) => {
+        try {
+            const { username, email, password, confirmPassword } = request.body;
+
+            const result = await userService.registerUser({ username, email, password, confirmPassword });
+
+            return response.status(result.status).json({ message: result.message });
+        } catch (error) {
+            console.error('UserController::registerUser ', error);
+            response.status(500).json({ message: 'Something went wrong! Try again.' });
+        }
+    }
+
+    loginUser = async (request, response) => {
+        try {
+            const { email, password } = request.body;
+            const result = await userService.loginUser({ email, password });
+    
+            return response.status(result.status).json({ message: result.message });
+        } catch (error) {
+            console.error('UserController::loginUser ', error);
+            response.status(500).json({ message: 'Something went wrong! Try again.' });
+        }   
+    }
+
+    getUsers = async (request, response) => {
+        try {
+            const users = await userService.getUsers();
+
+            if (! users || users.length === 0) {
+                return response.status(404).json({ message: 'No users found!' });
+            } else if (Object.keys(users).length === 0) {
+                return response.status(204).json({ message: 'No users found!' });
+            }
+
+            return response.status(200).json({ users: users });
+        } catch (error) {
+            console.error('UserController::getUsers ', error);
+            response.status(500).json({ message: 'Something went wrong! Try again.' });
+        }
+    }
+
+    findUser = async (request, response) => {
+        try {
+            const user = await userService.findUser(request.params.id);
+
+            if (! user) {
+                return response.status(404).json({ message: 'User not found!' });
+            } else if (Object.keys(user).length === 0) {
+                return response.status(204).json({ message: 'User not found!' });
+            }
+
+            return response.status(200).json({ user: user });
+        } catch (error) {
+            console.error('UserController::findUser ', error);
+            response.status(500).json({ message: 'Something went wrong! Try again.' });
+        }
+    }
+}
