09d6b2220e22adacefb9aec8eb69652e74059b76
Kevin Lucas <kevinbrito2012@gmail.com>
2023-10-12 11:22:08 -0300

    fix: change the route name

--- backend/router/applicationRoutes.js
+++ backend/router/applicationRoutes.js
@@ -4,8 +4,7 @@ const userController = new UserController();
 
 router.post('/register', (request, response) => userController.registerUser(request, response));
 router.post('/login', (request, response) => userController.loginUser(request, response));
-router.get('launches', (request, response) => userController.getUsers(request, response));
-router.get('launches/:id', (request, response) => userController.findUser(request, response));
-
+router.get('/users', (request, response) => userController.getUsers(request, response));
+router.get('/users/:id', (request, response) => userController.findUser(request, response));
 
 module.exports = router;
