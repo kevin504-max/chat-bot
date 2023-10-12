25aaabd64a962e3571c49f2c712e215f38d0d394
Kevin Lucas <kevinbrito2012@gmail.com>
2023-10-12 11:07:45 -0300

    chore: call the user functions in router file

--- backend/router/applicationRoutes.js
+++ backend/router/applicationRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
+const UserController = require('../controllers/UserController');
+const userController = new UserController();
+
+router.post('/register', (request, response) => userController.registerUser(request, response));
+router.post('/login', (request, response) => userController.loginUser(request, response));
+router.get('launches', (request, response) => userController.getUsers(request, response));
+router.get('launches/:id', (request, response) => userController.findUser(request, response));
+
 
 module.exports = router;
