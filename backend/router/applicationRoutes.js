1e649eb1a2b7ed6f7169bc1b5119e9d55f7e884d
Kevin Lucas <kevinbrito2012@gmail.com>
2023-10-15 11:54:50 -0300

    chore: call the send message route

--- backend/router/applicationRoutes.js
+++ backend/router/applicationRoutes.js
@@ -2,10 +2,15 @@ const router = require('express').Router();
 const UserController = require('../controllers/UserController');
 const userController = new UserController();
 
+const MessageController = require('../controllers/MessageController');
+const messageController = new MessageController();
+
 router.post('/register', (request, response) => userController.registerUser(request, response));
 router.post('/login', (request, response) => userController.loginUser(request, response));
 router.get('/users', (request, response) => userController.getUsers(request, response));
 router.get('/users/:id', (request, response) => userController.findUser(request, response));
 router.get('/users/:username/messages', (request, response) => userController.getUserMessages(request, response));
+
+router.post('/:chatId/send-message', (request, response) => messageController.sendMessage(request, response));
 
 module.exports = router;
