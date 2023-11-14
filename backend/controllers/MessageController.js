99ec23fea8592cbf636b0d9367e3459c77b16e8f
Kevin Lucas <kevinbrito2012@gmail.com>
2023-11-14 10:09:04 -0300

    feat: make hash game for external interface

--- backend/controllers/MessageController.js
+++ backend/controllers/MessageController.js
@@ -1,8 +1,18 @@
-const { MessageService } = require('../services');
-const messageService = new MessageService();
+const { BotService, HashGameService, MessageService } = require('../services');
 
-const { BotService } = require('../services');
+const messageService = new MessageService();
 const botService = new BotService();
+const hashGameService = new HashGameService();
+
+const PLAY_COMMAND = '/hashGame';
+const USER_SYMBOLS = ['X', 'O', 'x', 'o'];
+const MOVIMENTS = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const CHOICES = ['X', 'O'];
+
+let firstTurn = '';
+let robotChoice = '';
+let userChoice = '';
+let isPlaying = false;
 
 module.exports = class MessageController {
     sendMessage = async (req, res) => {
@@ -13,10 +23,34 @@ module.exports = class MessageController {
 
             const response = await messageService.sendMessage({ username, message, sendTo, chatId });
 
-            // Build the bot message to user in the external plataform and save it to the database
-            await botService.buildBotMessage(message, chatId, username).then((response) => {
-                messageService.saveBotMessage(response, username, chatId);
-            });
+            if (message === PLAY_COMMAND) {
+                isPlaying = true;
+                messageService.saveBotMessage('Choose your symbol: X or O', username, chatId);
+            }
+
+            if (isPlaying) {
+                if (robotChoice === '' && userChoice === '') {
+                    const symbol = message.toUpperCase();
+                    const selectedSymbol = USER_SYMBOLS.includes(symbol) ? symbol : null;
+
+                    if (selectedSymbol) {
+                        robotChoice = (symbol === CHOICES[0]) ? CHOICES[1] : CHOICES[0];
+                        userChoice = symbol;
+
+                        messageService.saveBotMessage(`You chose ${symbol}!\nDo you want to start? (Y/N):`, username, chatId);
+                    }
+                } else if (['Y', 'N'].includes(message.toUpperCase())) {
+                    firstTurn = message.toUpperCase();
+                    await hashGameService.gameLoop(robotChoice, userChoice, firstTurn, chatId, username);
+                } else if (MOVIMENTS.includes(message)) {
+                    await hashGameService.UserTurn(robotChoice, userChoice, chatId, message, username);
+                }
+            }
+
+            if (!([PLAY_COMMAND, ...USER_SYMBOLS, 'Y', 'N', 'y', 'n', ...CHOICES, ...MOVIMENTS].includes(message))) {
+                isPlaying = false;
+                await this.sendNonGameMessage(message, chatId, username);
+            }
 
             res.status(response.status).send(response.message);
         } catch (error) {
@@ -24,4 +58,9 @@ module.exports = class MessageController {
             res.status(500).send(`Error ${error}`);
         }
     }
-}
+
+    async sendNonGameMessage(message, chatId, username) {
+        const botMessage = await botService.buildBotMessage(message, chatId, username);
+        messageService.saveBotMessage(botMessage, username, chatId);
+    }
+};
